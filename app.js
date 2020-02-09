d4049705c26a56a79fe0a0ca47bc210cb46da086
Tuvshinbat <Tuvshinbat596@gmail.com>
2020-02-09 22:17:57 +0800

    Dice show

--- app.js
+++ app.js
@@ -12,20 +12,23 @@ var roundScore = 0;
 
 // Шооны аль талаарай буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
 
-var dice = Math.floor(Math.random() * 6) + 1;
+
 
 // document.querySelector('#score-0').textContent = dice;
 // document.querySelector('#score-1').innerHTML = "<em> Yes! </em>";
 
 // Програм эхлэхэд бэлтгэе 
-document.querySelector(".dice").style.display = "none";
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
 
-document.querySelector('#score-0').textContent = 0;
-document.querySelector('#score-1').textContent = 0;
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
-document.querySelector('#current-0').textContent = 0;
-document.querySelector('#current-1').textContent = 0;
+document.querySelector(".btn-roll").addEventListener("click", function(){
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
 
-
-
-console.log('Шоо :' + dice)
+    diceDom.style.display = "block";
+    diceDom.src = 'dice-' + diceNumber + '.png';
+});
